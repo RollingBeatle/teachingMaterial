9be78692f8b7762adf1a9cb74b5abb1a3f1fe8d3
Diego Ortiz <diego.ortiz.barbosa94@gmail.com>
2023-10-09 11:35:36 -0700

    added new functions

--- Calculator.py
+++ Calculator.py
@@ -1,20 +1,30 @@
+#Basic Structure of a python program
+
+#Imports
+
+#Global variables
+
+#Functions
+
+#Main classes
 
 
 def main():
 
     while True:
         print("Select operation:")
-        print("1. Add\n2. Subtract\n3. Multiply\n4. Divide\n5. Exit")
+        print("1. Add\n2. Subtract\n3. Multiply\n4. Divide\n5. square number\n6. Floor division \n7. Factorial\n8. Exit")
 
-        option = input("Enter option (1/2/3/4/5): ")
+        option = input("Enter option (1/2/3/4/5/6/7/8): ")
 
-        if option == '5':
-            print("Exiting the calculator. Goodbye!")
+        if option == '8':
+            print("Exiting the calculator")
             break
 
-        if option in ('1', '2', '3', '4'):
+        if option in ('1', '2', '3', '4','5','6','7','8'):
             num1 = float(input("Enter first number: "))
-            num2 = float(input("Enter second number: "))
+            if option !='5'and option !='7':
+                num2 = float(input("Enter second number: "))
 
             if option == '1':
                 print(f"{num1} + {num2} = {num1+ num2}")
@@ -25,9 +35,24 @@ def main():
             elif option == '3':
                 print(f"{num1} * {num2} = {num1*num2}")
 
-            elif option == '4':
-                
+            elif option == '4':    
                 print(f"{num1} / {num2} = {num1/num2}")
+
+            elif option =='5':
+                print(f'{num1}  ** 2 = {num1*num1}')
+
+            elif option =='6':
+                print(f'{num1} // {num2} = {num1//num2}')
+
+            elif option == '7':
+                result = 1
+                res = 1
+                for i in range(1, int(num1) + 1):
+                    result *= i
+                for i in range(int(num1), 1, -1):
+                    res *=i
+                print(f"{res}")
+                print(f"{num1}! = {result}")
 
         else:
             print("Invalid input. Please enter a valid number.")
